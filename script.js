681b89f42723ed871983a9825bcd52be5b70b269
Brendan Gallagher <bpgallagher27@gmail.com>
2020-01-17 18:22:19 -0500

    basic setup

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var passwordLength = prompt("Please choose a password length (8 - 128 characters)");
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var numbers = "0123456789";
-var symbols = "!@#$%&*+-:;_";
-var all = upperCase + lowerCase + numbers + symbols;
 
 if (passwordLength >=8) {
-    writePassword;
+    
 } else {
     alert("Please choose a length between 8 and 128");
 }
 
 if (passwordLength <=128) {
-    writePassword;
+    
 } else {
     alert("Please choose a length between 8 and 128");
 }
+
 
 // Write password to the #password input
 function writePassword() {
